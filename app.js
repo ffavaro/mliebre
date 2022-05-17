682d9207aa32b5ade3846f45d9ad0c98a5e44b2b
francisco.favaro <ffavaro@escrbianos.org>
2022-05-16 22:29:00 -0300

    inicio

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicPath =  path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath));
+
+app.listen(3000, (req, res) => {
+    console.log("Server on");
+});
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
+
+app.get('/home', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
